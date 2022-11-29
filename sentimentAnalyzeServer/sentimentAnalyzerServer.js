b529561a23eb125f56a95af127d1cd53dea97b00
dxliang001 <dongxinliang6@gmail.com>
2022-11-29 15:26:43 -0500

    create

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -12,16 +12,29 @@ app.use(cors_app());
 /*Uncomment the following lines to loan the environment 
 variables that you set up in the .env file*/
 
-// const dotenv = require('dotenv');
-// dotenv.config();
+ const dotenv = require('dotenv');
+ dotenv.config();
 
-// const api_key = process.env.API_KEY;
-// const api_url = process.env.API_URL;
 
 function getNLUInstance() {
     /*Type the code to create the NLU instance and return it.
     You can refer to the image in the instructions document
     to do the same.*/
+
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2022-11-29',
+        authenticator: new IamAuthenticator ({
+            apikey: api_key
+        }),
+        serviceUrl: api_url
+    });
+    return naturalLanguageUnderstanding;
 }
 
 
@@ -33,28 +46,28 @@ app.get("/",(req,res)=>{
 //The endpoint for the webserver ending with /url/emotion
 app.get("/url/emotion", (req,res) => {
     // //Extract the url passed from the client through the request object
-    // let urlToAnalyze = req.query.url
-    // const analyzeParams = 
-    //     {
-    //         "url": urlToAnalyze,
-    //         "features": {
-    //             "keywords": {
-    //                             "emotion": true,
-    //                             "limit": 1
-    //                         }
-    //         }
-    //     }
+     let urlToAnalyze = req.query.url
+     const analyzeParams = 
+         {
+             "url": urlToAnalyze,
+             "features": {
+                 "keywords": {
+                                 "emotion": true,
+                                 "limit": 1
+                             }
+             }
+         }
      
-    //  const naturalLanguageUnderstanding = getNLUInstance();
+      const naturalLanguageUnderstanding = getNLUInstance();
      
-    //  naturalLanguageUnderstanding.analyze(analyzeParams)
-    //  .then(analysisResults => {
+      naturalLanguageUnderstanding.analyze(analyzeParams)
+      .then(analysisResults => {
     //     //Please refer to the image to see the order of retrieval
-    //     return res.send(analysisResults.result.keywords[0].emotion,null,2);
-    //  })
-    //  .catch(err => {
-    //  return res.send("Could not do desired operation "+err);
-    //  });
+         return res.send(analysisResults.result.keywords[0].emotion,null,2);
+      })
+      .catch(err => {
+      return res.send("Could not do desired operation "+err);
+      });
 });
 
 //The endpoint for the webserver ending with /url/sentiment
@@ -75,3 +88,97 @@ let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
+
+function analyzeEmotionUrl(url,res){
+    const naturalLanguageUnderstanding= getNLUInstance();
+    const analyzeParams = {
+  'url':url,
+  'features': {
+    'emotion': {
+    }
+  }
+};
+naturalLanguageUnderstanding.analyze(analyzeParams)
+  .then(analysisResults => {
+    console.log(JSON.stringify(analysisResults, null, 2));
+   // return analysisResults.result.emotion.document.emotion;
+   res.send(analysisResults.result.emotion.document.emotion);
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+
+}
+
+
+function analyzeEmotionText(sampleText,res){
+    const naturalLanguageUnderstanding= getNLUInstance();
+    const analyzeParams = {
+  'html':sampleText,
+  'features': {
+    'emotion': {     
+    }
+  }
+};
+naturalLanguageUnderstanding.analyze(analyzeParams)
+  .then(analysisResults => {
+    console.log(JSON.stringify(analysisResults, null, 2));
+   // return analysisResults.result.emotion.document.emotion;
+   res.send(analysisResults.result.emotion.document.emotion);
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+
+}
+
+
+function analyzeSentimentText(sampleText,res){
+    const naturalLanguageUnderstanding= getNLUInstance();
+      
+   const analyzeParams = {
+      'html': sampleText,
+       'features': {
+        'sentiment': {
+          
+        }
+       }
+    };
+
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+      .then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2));
+        //.result.entities[0].sentiment
+         res.send(analysisResults.result.sentiment.document.label);
+        
+      })
+      .catch(err => {
+          
+        console.log('error:', err);
+         res.send("nutral");
+      });
+
+}
+function analyzeSentimentUrl(url,res){
+    const naturalLanguageUnderstanding= getNLUInstance();
+      
+   const analyzeParams = {
+      'url': url,
+       'features': {
+        'sentiment': {
+          
+        }
+       }
+    };
+
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+      .then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2));
+         res.send(analysisResults.result.sentiment.document.label);
+      })
+      .catch(err => {
+        console.log('error:', err);
+         res.send("nutral");
+      });
+
+}
